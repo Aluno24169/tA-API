1b3595c3831328a8936abd2009ad77893994a3f6
Aluno24169 <aluno24169@ipt.pt>
2026-03-11 10:21:56 +0000

    Adição de sumários às classes.

--- API/API/Models/MyUser.cs
+++ API/API/Models/MyUser.cs
@@ -1,13 +1,44 @@
 ﻿namespace API.Models
 {
+    
+    /// <summary>
+    /// Dados dos clientes da loja
+    /// </summary>
     public class MyUser
     {
+        /// <summary>
+        /// PK
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Nome do cliente
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Morada do Cliente
+        /// </summary>
         public string Address { get; set; }
+
+        /// <summary>
+        /// Código postal do cliente
+        /// </summary>
         public string PostalCode { get; set; }
+
+        /// <summary>
+        /// País da morada do cliente
+        /// </summary>
         public string Country { get; set; }
+
+        /// <summary>
+        /// Número de contribuinte
+        /// </summary>
         public string TaxNumber { get; set; }
+
+        /// <summary>
+        /// Número de telemovel do cliente
+        /// </summary>
         public string CellPhone { get; set; }
     }
 }
