2fa095f28df0f0c92868b6cc732786de5469914f
Aluno24169 <aluno24169@ipt.pt>
2026-03-11 10:06:37 +0000

    Classes iniciais do model.

---
+++ API/API/Models/Purchase.cs
@@ -0,0 +1,18 @@
+﻿namespace API.Models
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public State State { get; set; }
+    }
+
+    public enum State
+    {
+        Pending,
+        Paid,
+        Sent,
+        Delivered,
+        Closed
+    }
+}
